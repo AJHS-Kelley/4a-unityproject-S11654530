23950e982214a660967df8bcf0ab1b5ea3b16c73
Ronald Durham <70582212+S11654530@users.noreply.github.com>
2024-02-06 14:08:45 -0800

    Update Program.cs

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
+       int myInt = 0;
+       float myFloat = 3.14F
+       bool myBool = true;
+       string myString = "I love basketball.\n";
 
+    Console.WriteLine("My Interger variable is "+myint);
+    Console.WriteLine("My Float variable is "+myFloat);
+    Console.WriteLine("My Boolean variable is "+mybool);
+    Console.WriteLine("My String variable is "+myString);
+
+    for (int i = 0; i<= 20; i++)
+    Console.WriteLine("i ate "+i +"slices of pizza for lunch.\n");
     }
 }
